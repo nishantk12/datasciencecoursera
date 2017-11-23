b64e3991fd46ae72ad21bd795f3f4ab86c60d1f8
Nishant Kumar <nishant_kumar-nkc@mckinsey.com>
2017-11-23 00:30:52 -0500

    fixing coal plot

--- Exploratory_Data_Analysis/course_project/code/plot4.R
+++ Exploratory_Data_Analysis/course_project/code/plot4.R
@@ -3,7 +3,7 @@ pol <- readRDS("~/Documents/coursera/Data-science-spec/4/course-project/exdata%2
 pol1 <- readRDS("~/Documents/coursera/Data-science-spec/4/course-project/exdata%2Fdata%2FNEI_data/Source_Classification_Code.rds")
 
 #create the coal_source 
-coal_source <- pol1 %>% filter(grepl("Coal", Short.Name))
+coal_source <- pol1 %>% filter(grepl("Coal", EI.Sector))
 
 #get vector of all the sources of coal
 coal_scc <- coal_source$SCC
@@ -15,6 +15,6 @@ pol_col <- filter(pol, SCC %in% coal_scc)
 year_total_pol_coal <- aggregate(Emissions ~ year, pol_col, sum)
 
 #plot the line over the year 
-plot(year_total_pol_coal$year, year_total_pol_coal$Emissions,col="blue",type="l",xlab="Years", ylab="Emission from all sources over the years")
+plot(year_total_pol_coal$year, year_total_pol_coal$Emissions,col="blue",type="l",xlab="Years", ylab="Emission from coal sources over the years")
 points(year_total_pol_coal$year, year_total_pol_coal$Emissions, col="red", pch=20)
 title(main="Total emission change over the years from coal sources")
